e9e081aad8c7ae7fbe8384821e509774c411062d
aspowerr <31863149+aspowerr@users.noreply.github.com>
2018-10-28 02:44:32 +0300

    passed not all tests

--- make.js
+++ make.js
@@ -1,5 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
 
 function make(...args) {
+    for(let i = 0; i < args.length; i++){
+    if (+args[i] !== args[i]) return 0;
+    }
+
     if (!args.length) return 0;
 
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
@@ -8,12 +15,17 @@ function make(...args) {
 
    return (...innerArgs) => {
 
-        if (innerArgs.length === 0) {
-            return result;
-        }
-         return make(...args, ...innerArgs);   
+         if (+innerArgs[0] !== innerArgs[0]) {
+             return result;
+         }
+          return make(...args, ...innerArgs);   
+
     }
 }
 
 
-console.log(make(15)(34, 21, 666)(41)());  
+console.log(make(15)(34, 21, 666)(41)(sum));  
+console.log(make(1,4,6)(666)(41,8)(sum));  
+console.log(make(15)(66)(1)(sum));  
+console.log(make(154)(sum));  
+console.log(make('hello'));  
